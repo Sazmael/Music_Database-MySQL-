00efd5b5b13bfd0a2d0762742077455118b993de
Olman Dávila Solano <85916689+Sazmael@users.noreply.github.com>
2022-05-24 21:01:05 -0600

    Script to update information and delete a song

---
+++ 4 Update_delete.sql
@@ -0,0 +1,9 @@
+use music;
+
+-- Change the name
+update band set name = 'Trees of Eternity' where id ='9';
+select *from band name where id = 9;
+
+delete from song where name = 'Clouds' and album_id = '28';
+select *from song where album_id = '28';
+
